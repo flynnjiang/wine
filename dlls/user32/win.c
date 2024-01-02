8470cb4af8b8c37c7a186e00e9371073fb49a8f3
Feng Jiang <jiangfeng@kylinos.cn>
2024-01-02 09:43:51 +0800

    user32: Hack wechat shadow border
    Signed-off-by: Feng Jiang <jiangfeng@kylinos.cn>

--- dlls/user32/win.c
+++ dlls/user32/win.c
@@ -454,6 +454,12 @@ HWND WINAPI DECLSPEC_HOTPATCH CreateWindowExW( DWORD exStyle, LPCWSTR className,
 {
     CREATESTRUCTW cs;
 
+    if (exStyle == 0x080800a0)
+    {
+	    FIXME("hack wechat shadow border issue.\n");
+	    return NULL;
+    }
+
     cs.lpCreateParams = data;
     cs.hInstance      = instance;
     cs.hMenu          = menu;
